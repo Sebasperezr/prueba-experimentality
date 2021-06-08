a36bccd0c580c3a6ad9c30ca206d5f225c0f6266
Sebastian Perez <sebasperezr@gmail.com>
2021-06-07 21:53:46 -0500

    Base home

--- app/src/app/component/component.module.ts
+++ app/src/app/component/component.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductsMostWantedComponent } from './products-most-wanted/products-most-wanted.component';
+import { SlideComponent } from './slide/slide.component';
 
 @NgModule({
-  declarations: [FooterComponent, NavBarComponent, HeaderComponent],
+  declarations: [
+    FooterComponent,
+    NavBarComponent,
+    HeaderComponent,
+    SlideComponent,
+    ProductsMostWantedComponent,
+  ],
   imports: [CommonModule],
-  exports: [FooterComponent, NavBarComponent, HeaderComponent],
+  exports: [
+    FooterComponent,
+    NavBarComponent,
+    HeaderComponent,
+    SlideComponent,
+    ProductsMostWantedComponent,
+  ],
 })
 export class ComponentModule {}
---
+++ app/src/app/component/products-most-wanted/products-most-wanted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-most-wanted',
+  templateUrl: './products-most-wanted.component.html',
+  styleUrls: ['./products-most-wanted.component.scss']
+})
+export class ProductsMostWantedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
