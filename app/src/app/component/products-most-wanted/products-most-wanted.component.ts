76a7dfcd90d23c08b21db5dfb517cef3edfa2b16
Sebastian Perez <sebasperezr@gmail.com>
2021-06-08 23:42:22 -0500

    show products

--- app/src/app/component/products-most-wanted/products-most-wanted.component.ts
+++ app/src/app/component/products-most-wanted/products-most-wanted.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products-most-wanted',
   templateUrl: './products-most-wanted.component.html',
-  styleUrls: ['./products-most-wanted.component.scss']
+  styleUrls: ['./products-most-wanted.component.scss'],
 })
 export class ProductsMostWantedComponent implements OnInit {
-
-  constructor() { }
+  products = [];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    console.log('init');
+
+    this.listOffererAuctions();
   }
 
+  listOffererAuctions() {
+    this.productService.listMostWanted().subscribe(
+      (data) => {
+        this.products = data.results;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
